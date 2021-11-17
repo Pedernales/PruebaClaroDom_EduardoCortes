04764fd24b5b317348633af374997ee128b10241
Pedernales <eduardo.leo.c@gmail.com>
2021-11-17 03:20:56 -0400

    Add files via upload

---
+++ PruebaClaroDom_EduardoCortes/PruebaClaroDom_EduardoCortes/Controllers/BooksController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PruebaClaroDom_EduardoCortes.Model;
+using System.Net.Http;
+
+
+namespace PruebaClaroDom_EduardoCortes.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BooksController : ControllerBase
+    {
+        HttpClient cliente = new HttpClient();
+
+        public BooksController()
+        {
+            
+        }
+
+        private async Task GetBooksTask()
+        {
+            string response = await cliente.GetStringAsync("https://fakerestapi.azurewebsites.net/api/v1/Books");
+        }
+
+        [HttpGet]
+        public IEnumerable<BooksModel> GetBooks()
+        {
+            return new List<BooksModel>();
+        }
+
+        [HttpGet("{_id}")]
+        public BooksModel GetBook(int _id)
+        {
+
+
+            return new BooksModel();
+        }
+    }
+}
